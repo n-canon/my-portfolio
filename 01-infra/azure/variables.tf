2a2d3f425f24a67feca69a5b06d21d6f7bb1e5f1
snatch <snatchcnn@nca.aleeas.com>
2025-02-26 01:22:41 +0100

    push

--- 01-infra/azure/variables.tf
+++ 01-infra/azure/variables.tf
@@ -1,32 +1,38 @@
 variable "location" {
   description = "Ressources location"
   type        = string
+  default = "West Europe"
 }
 
 variable "environment" {
   description = "Environment of creation of the ressources"
   type        = string
+  default = "dvp"
 }
 
 
 variable "project_name" {
   description = "Name of the project"
   type        = string
+  default = "nca-project"
 }
 
 
 variable "ad_admin_group" {
   description = "Name of admin group"
   type        = string
+  default = "az-developers-dvp"
 }
 
 
 variable "sql_admin_user" {
   description = "Username of SQL database Admin user"
   type        = string
+  default = "sa_poc_test"
 }
 
 variable "sql_admin_password" {
   description = "Password of SQL database Admin user"
   type        = string
+  default = "test"
 }
