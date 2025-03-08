194e16acaa1aab6a434625c8179f9093a8f52f24
cnatch <canonnatchley@gmail.com>
2025-03-08 19:54:40 +0100

    Add set -PsReposity to authorize new installation

--- 01-infra/devops/adf/deploy_adf.ps1
+++ 01-infra/devops/adf/deploy_adf.ps1
@@ -1,3 +1,4 @@
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module -Name azure.datafactory.tools -Scope CurrentUser
 Import-Module -Name azure.datafactory.tools
 
@@ -6,3 +7,5 @@ Publish-AdfV2FromJson
    -ResourceGroupName     $Env:ResourceGroupName
    -DataFactoryName       $Env:DataFactoryName
    -Location              $Env:Location
+
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
