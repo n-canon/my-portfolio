1f6ff248975f7eec807a3f9a1b87ddbad7d2da09
cnatch <canonnatchley@gmail.com>
2025-03-08 19:58:40 +0100

    add az module

--- 01-infra/devops/adf/deploy_adf.ps1
+++ 01-infra/devops/adf/deploy_adf.ps1
@@ -1,4 +1,6 @@
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module Az -RequiredVersion 10.0.0 -Scope CurrentUser -Force -AllowClobber
+Import-Module Az -RequiredVersion 10.0.0 -Force
 Install-Module -Name azure.datafactory.tools -Scope CurrentUser
 Import-Module -Name azure.datafactory.tools
 
