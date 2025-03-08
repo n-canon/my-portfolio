5219fa957138daa7007d0e0f1229e4ee5f71a0eb
cnatch <canonnatchley@gmail.com>
2025-03-08 23:37:58 +0100

    update pwsl command

--- 01-infra/devops/adf/deploy_adf.ps1
+++ 01-infra/devops/adf/deploy_adf.ps1
@@ -4,10 +4,6 @@ Import-Module Az -RequiredVersion 10.0.0 -Force
 Install-Module -Name azure.datafactory.tools -Scope CurrentUser
 Import-Module -Name azure.datafactory.tools
 
-Publish-AdfV2FromJson 
-   -RootFolder            $env:adf_root
-   -ResourceGroupName     $env:rg_name
-   -DataFactoryName       $env:data_factory_name
-   -Location              $env:rg_location
+Publish-AdfV2FromJson -RootFolder "$env:adf_root" -ResourceGroupName "$env:rg_name" -DataFactoryName "$env:data_factory_name" -Location "$env:rg_location"
 
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
