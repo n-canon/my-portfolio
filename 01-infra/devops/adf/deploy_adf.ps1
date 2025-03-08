07626b5d8ca2775196f62b7e01b4781c9b785fec
cnatch <canonnatchley@gmail.com>
2025-03-08 20:04:23 +0100

    modify variables

--- 01-infra/devops/adf/deploy_adf.ps1
+++ 01-infra/devops/adf/deploy_adf.ps1
@@ -5,9 +5,9 @@ Install-Module -Name azure.datafactory.tools -Scope CurrentUser
 Import-Module -Name azure.datafactory.tools
 
 Publish-AdfV2FromJson 
-   -RootFolder            $Env:RootFolder
-   -ResourceGroupName     $Env:ResourceGroupName
-   -DataFactoryName       $Env:DataFactoryName
-   -Location              $Env:Location
+   -RootFolder            $Env:adf_root
+   -ResourceGroupName     $Env:rg_name
+   -DataFactoryName       $Env:data_factory_name
+   -Location              $Env:rg_location
 
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
