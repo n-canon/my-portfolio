59072bdeb273c752841157b6428fc9d0f66e7d75
cnatch <canonnatchley@gmail.com>
2025-03-17 00:01:28 +0100

    update reference to secret variable

--- 01-infra/devops/keyvault/deploy_secrets.ps1
+++ 01-infra/devops/keyvault/deploy_secrets.ps1
@@ -6,23 +6,24 @@ Connect-AzAccount
 
 $secrets = Import-CSV -Path "./secrets/secrets.csv"
 
-#Extract keyvault list before creating/updating values
+#Creating/updating secrets
 $lst_keyvault_secrets = (Get-AzKeyVaultSecret -Vaultname "$env:kvname").name
-#Create or update secrets
 For($i=0 ; $i -lt $secrets.Length; $i++) 
 { 
-    if (-not($lst_keyvault_secrets -contains $secrets.name[$i]) ){
+    if (-not($lst_keyvault_secrets -contains $secrets.name[$i]) )
+    {
         Write-Output "Creating/Updating secret $secrets.name[$i])" 
         $secret = ConvertTo-SecureString -String $secrets.value[$i] -AsPlainText -Force
         Set-AzKeyVaultSecret -VaultName "$env:kvname" -Name $secrets.name[$i] -SecretValue $secret
     }
 }
 
-#Extract keyvault list for deleting values
+#Delete secrets
 $lst_keyvault_secrets = (Get-AzKeyVaultSecret -Vaultname "$env:kvname").name
 For($i=0 ; $i -lt $lst_keyvault_secrets.Length; $i++) 
 { 
-    if (-not($secrets.name -contains $lst_keyvault_secrets[$i]) ){
+    if (-not($secrets.name -contains $lst_keyvault_secrets[$i]) )
+    {
         Write-Output "Deleting secret $lst_keyvault_secrets[$i])"   
         Remove-AzKeyVaultSecret -VaultName "$env:kvname" -Name $lst_keyvault_secrets[$i] -PassThru -Force
     }
