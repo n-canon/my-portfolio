7980cfc317546c65bada7af97b925f66f47ccc74
cnatch <canonnatchley@gmail.com>
2025-03-17 13:00:32 +0100

    deploy secrets

--- 01-infra/devops/keyvault/deploy_secrets.ps1
+++ 01-infra/devops/keyvault/deploy_secrets.ps1
@@ -1,21 +1,33 @@
+Param(
+    [Parameter(Mandatory=$True)]
+    [String] $secretFile
+)
+
 Install-Module -Name Microsoft.PowerShell.SecretManagement -Repository PSGallery -Force
 Install-Module Az -RequiredVersion 10.0.0 -Scope CurrentUser -Force -AllowClobber
 Import-Module Az -RequiredVersion 10.0.0 -Force
 Import-Module Microsoft.PowerShell.SecretManagement
-Connect-AzAccount
+#Connect-AzAccount
 
-$secrets = Import-CSV -Path "./secrets/secrets.csv"
+$secrets = Import-CSV -Path $secretFile
 
 #Creating/updating secrets
-$lst_keyvault_secrets = (Get-AzKeyVaultSecret -Vaultname "$env:kvname").name
 For($i=0 ; $i -lt $secrets.Length; $i++) 
 { 
-    #if (-not($lst_keyvault_secrets -contains $secrets.name[$i]) )
-    #{
-        Write-Output "Creating/Updating secret $secrets.name[$i])" 
+    if ($lst_keyvault_secrets -contains $secrets.name[$i] )
+    {
+        $old_secret = Get-AzKeyVaultSecret -VaultName "$env:kvname" -Name $secrets.name[$i] -AsPlainText
+        if ($old_secret -ne $secrets.value[$i]) {
+            Write-Output "Updating secret $secrets.name[$i]" 
+            $secret = ConvertTo-SecureString -String $secrets.value[$i] -AsPlainText -Force
+            Set-AzKeyVaultSecret -VaultName "$env:kvname" -Name $secrets.name[$i] -SecretValue $secret
+        }     
+    }
+    else {      
+        Write-Output "Creating secret $secrets.name[$i]" 
         $secret = ConvertTo-SecureString -String $secrets.value[$i] -AsPlainText -Force
         Set-AzKeyVaultSecret -VaultName "$env:kvname" -Name $secrets.name[$i] -SecretValue $secret
-    #}
+    }
 }
 
 #Delete secrets
@@ -24,7 +36,7 @@ For($i=0 ; $i -lt $lst_keyvault_secrets.Length; $i++)
 { 
     if (-not($secrets.name -contains $lst_keyvault_secrets[$i]) )
     {
-        Write-Output "Deleting secret $lst_keyvault_secrets[$i])"   
+        Write-Output "Deleting secret $lst_keyvault_secrets[$i]"   
         Remove-AzKeyVaultSecret -VaultName "$env:kvname" -Name $lst_keyvault_secrets[$i] -PassThru -Force
     }
 }
