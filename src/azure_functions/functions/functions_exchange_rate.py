77c642b02f859b30749eea8d89ffd2815cf69400
cnatch <canonnatchley@gmail.com>
2025-03-19 22:35:10 +0100

    push function code

--- src/azure_functions/functions/functions_exchange_rate.py
+++ src/azure_functions/functions/functions_exchange_rate.py
@@ -7,21 +7,31 @@ bp_exchange_rate = func.Blueprint()
 @bp_exchange_rate.route(route="exchange_rate")
 @bp_exchange_rate.function_name(name="Mysecondfunction") 
        
-def exchange_rate_to_blob(req: func.HttpRequest)-> func.HttpResponse:
-
-    #Get api key
-    kvClient = keyvaultClient('kv-ncaproject-dvp')
-    exchange_rate_key = kvClient.get_secret("exchange-rate-api-key")
-     
-    # send request and get response
-    url = 'https://v6.exchangerate-api.com/v6/'+exchange_rate_key+'/latest/USD'
-    data = requests.get(url).json()
+def exchange_rate_to_blob(req: func.HttpRequest) -> func.HttpResponse:
     
-    #save file to blob storage
-    blobClient = blobServiceClient("stncaprojectdvp")
-    blobClient.upload_blob_stream("landing","exchange_rate/test3.json",str(data).encode())
+    name = req.params.get('name')
+    if not name:
+        try:
+             #Get api key
+            kvClient = keyvaultClient('kv-ncaproject-dvp')
+            exchange_rate_key = kvClient.get_secret("exchange-rate-api-key")
+            
+            # send request and get response
+            url = 'https://v6.exchangerate-api.com/v6/'+exchange_rate_key+'/latest/USD'
+            data = requests.get(url).json()
+            
+            #save file to blob storage
+            blobClient = blobServiceClient("stncaprojectdvp")
+            blobClient.upload_blob_stream("landing","exchange_rate/test3.json",str(data).encode())
+        except ValueError:
+            pass
 
-    return func.HttpResponse(
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+   
