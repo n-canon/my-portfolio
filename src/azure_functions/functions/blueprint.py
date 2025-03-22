3228515a9e670495b15b1cdfa2036c8d71dd6aeb
cnatch <canonnatchley@gmail.com>
2025-03-22 21:08:53 +0100

    comment non working function

--- src/azure_functions/functions/blueprint.py
+++ src/azure_functions/functions/blueprint.py
@@ -3,7 +3,7 @@ import logging
 
 blueprint = func.Blueprint()
 
-
+@blueprint.function_name(name="Mytimer")
 @blueprint.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
