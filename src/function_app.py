b39b0a45ff3e023320df7eba985b2903f85ec967
cnatch <canonnatchley@gmail.com>
2025-03-22 20:51:55 +0100

    add new function

--- src/function_app.py
+++ src/function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
 from azure_functions.functions.MyFirstFunction import bp
 from azure_functions.functions.Mysecondfunction import bp_exchange_rate
+from azure_functions.functions.blueprint import blueprint
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
 app.register_functions(bp) 
 app.register_functions(bp_exchange_rate) 
+app.register_functions(blueprint) 
