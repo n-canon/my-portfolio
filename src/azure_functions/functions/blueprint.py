7644983d787b012cfd8a354c95a7020720facf64
cnatch <canonnatchley@gmail.com>
2025-03-22 21:48:54 +0100

    rename file

--- src/azure_functions/functions/blueprint.py
+++ src/azure_functions/functions/blueprint.py
@@ -1,10 +1,10 @@
 import azure.functions as func
 import logging
 
-blueprint = func.Blueprint()
+blueprintfunc = func.Blueprint()
 
-@blueprint.function_name(name="Mytimer")
-@blueprint.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
+@blueprintfunc.function_name(name="Mytimer")
+@blueprintfunc.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
