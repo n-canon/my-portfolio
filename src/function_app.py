61c4268eed0878b4856455e7f2bf1f5bdfa327cd
cnatch <canonnatchley@gmail.com>
2025-04-21 18:49:13 +0200

    remove src

--- src/function_app.py
+++ src/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
-from src.azure_functions.functions.ApiExchangeRate import bp_exchange_rate
-from src.azure_functions.functions.ApiCountry import bp_country
+from azure_functions.functions.ApiExchangeRate import bp_exchange_rate
+from azure_functions.functions.ApiCountry import bp_country
 from azure_functions.functions.blueprint import blueprintfunc
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
