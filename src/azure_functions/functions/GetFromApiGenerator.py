c72e368347580fb9669fe8a07ddebc2284686b73
cnatch <canonnatchley@gmail.com>
2026-01-18 00:18:51 +0100

    Update function code

--- src/azure_functions/functions/GetFromApiGenerator.py
+++ src/azure_functions/functions/GetFromApiGenerator.py
@@ -1,51 +1,43 @@
-import azure.functions as func
-import requests 
-import json
-from shared.infrastructure.blob_client import blobServiceClient
-from shared.infrastructure.keyvault_client import keyvaultClient
-from datetime import datetime
-from config.ApiConfig import apiConfig
+# import azure.functions as func
+# import requests 
+# import json
+# from shared.infrastructure.blob_client import blobServiceClient
+# from shared.infrastructure.keyvault_client import keyvaultClient
+# from datetime import datetime
+# from .config.ApiConfig import apiConfig
 
-kvClient = keyvaultClient('kv-ncaproject-dvp')
-blobClient = blobServiceClient("stncaprojectdvp")
+# kvClient = keyvaultClient('kv-ncaproject-dvp')
+# blobClient = blobServiceClient("stncaprojectdvp")
 
-def create_blueprint(item_name : str):
-    bp = func.Blueprint()
-    @bp.route(route=item_name)
-    @bp.function_name(name=item_name)
+# def create_blueprint(item_name : str):
+#     bp = func.Blueprint()
+#     @bp.route(route=item_name)
+#     @bp.function_name(name=item_name)
     
-    def api_handler(req: func.HttpRequest) -> func.HttpResponse:
+#     def api_handler(req: func.HttpRequest) -> func.HttpResponse:
         
-        name = req.params.get('name')
-        if not name:
-            try:
-                # send request and get response
-                url = item.url + item.relativeUrl # => ajouter une méthode de construction d'url
-                data = requests.get(url).json()
-                filename = item.sinkFilenamePattern+datetime.now().strftime("%Y%m%d%H%M%S")+'.json'      
-                #save file to blob storage
-                blobClient.upload_blob_stream(item.sinkBlobContainer,item.sinkDirectory+"/"+filename,json.dumps(data))               
-            except ValueError:
-                pass
-        if name:
-            return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-        else:
-            return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-                status_code=200
-            )
+#         name = req.params.get('name')
+#         if not name:
+#             try:
+#                 # send request and get response
+#                 url = item.url + item.relativeUrl # => ajouter une méthode de construction d'url
+#                 data = requests.get(url).json()
+#                 filename = item.sinkFilenamePattern+datetime.now().strftime("%Y%m%d%H%M%S")+'.json'      
+#                 #save file to blob storage
+#                 blobClient.upload_blob_stream(item.sinkBlobContainer,item.sinkDirectory+"/"+filename,json.dumps(data))               
+#             except ValueError:
+#                 pass
+#         if name:
+#             return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+#         else:
+#             return func.HttpResponse(
+#                 "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#                 status_code=200
+#             )
 
-    return bp
+#     return bp
 
-def generate_url(item_name: str,source : str) -> str :
-    if source == "exchange_rate":
-        secret_key = kvClient.get_secret(apiConfig.item_name.secretKey)
-        return apiConfig.item_name.url+secret_key
-
-
-blueprints = {}
-
-for item in apiConfig:
-    bp_name = f'bp_{item.name}'
-    blueprints[bp_name] = create_blueprint(item.name) 
-  
+# def generate_url(item_name: str,source : str) -> str :
+#     if source == "exchange_rate":
+#         secret_key = kvClient.get_secret(apiConfig.item_name.secretKey)
+#         return apiConfig.item_name.url+secret_key
--- src/function_app.py
+++ src/function_app.py
@@ -1,9 +1,9 @@
 import azure.functions as func
-from src.azure_functions.functions.config.ApiConfig import apiConfig
-from src.azure_functions.functions.GetFromApiGenerator import create_blueprint
+from azure_functions.functions.ApiCountry import bp_country
+from azure_functions.functions.ApiExchangeRate import bp_exchange_rate
+from azure_functions.functions.blueprint import blueprintfunc
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
-
-for item in apiConfig:
-    bp = create_blueprint(item.name)
-    app.register_functions(bp)
+app.register_functions(bp_country)
+app.register_functions(bp_exchange_rate)
+#app.register_functions(blueprintfunc)
